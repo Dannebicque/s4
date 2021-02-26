43d1ff2b2ddce6bdbf477aa91e9014ec6e682c0b
dannebicque <david.annebicque@gmail.com>
2021-02-26 14:57:21 +0100

    relations

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -17,13 +17,18 @@ class PostController extends AbstractController
      *
      * @return Response
      */
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(
+        PostCategoryRepository $postCategoryRepository,
+        EntityManagerInterface $entityManager): Response
     {
+        $category = $postCategoryRepository->find(1);//on récupère une catégorie
+
         //Créer une page qui va sauvegarder un post avec le nom Post 1 à la date courante avec comme contenu Lorem ipsum et en enable à true.
         $post = new Post();
-        $post->setTitle('Post 1');
-        $post->setContext('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec semper tellus vitae ante euismod tempus. Quisque non est luctus, tempus felis quis, pellentesque nulla. Sed hendrerit nulla ut ullamcorper dignissim. Aliquam eleifend, mi a gravida ullamcorper, odio nisl volutpat diam, sit amet commodo purus massa et libero. Nunc in porttitor libero, sit amet elementum ex. Donec nisi tellus, malesuada porta porttitor auctor, consequat eget felis. Nunc rhoncus sapien imperdiet nisi mattis pharetra non sed augue. Curabitur rhoncus libero magna, egestas pretium ipsum dignissim at. Nullam vulputate ut tortor et dignissim. Pellentesque eget diam sed augue auctor mollis quis in turpis. Donec gravida justo at magna dictum, eget posuere nibh ultrices. Suspendisse at aliquam mauris. Nam iaculis ipsum justo, at viverra turpis vulputate id. Sed convallis faucibus mi. Mauris tempor ante a dignissim porta.');
+        $post->setTitle('Post 2');
+        $post->setContext('texte.');
         $post->setEnable(true);
+        $post->setCategory($category);//on associe la catégorie au post
         $entityManager->persist($post);
         $entityManager->flush();
 
